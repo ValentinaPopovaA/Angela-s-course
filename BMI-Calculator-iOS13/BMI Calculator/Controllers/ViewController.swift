6f6b20463be581bee8fafd296b0cbdcaab864ce5
Valentina Popova <info@valentina-popova.ru>
2024-06-07 11:17:33 +0300

    Lesson 125 completed

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -38,6 +38,11 @@ class ViewController: UIViewController {
         let bmi = weight / (height * height)
         
         print(bmi)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
     }
 }
 
