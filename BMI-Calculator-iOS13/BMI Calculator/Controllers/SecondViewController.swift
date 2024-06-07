ac981be649fe666af3156f50176d2f16a862472b
Valentina Popova <info@valentina-popova.ru>
2024-06-07 10:53:26 +0300

    Lesson 124 completed

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Валентина Попова on 07.06.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
